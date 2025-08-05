7eb1778cdfe91c5b9b7a4271a7d00dcba8a6cc47
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-08-05 16:39:45 -0700

    i keep forgetting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import UnoCSS from "@unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tds-strat-db/",
   plugins: [react(), UnoCSS()],
   server: { host: "0.0.0.0", port: 5173 },
 });
