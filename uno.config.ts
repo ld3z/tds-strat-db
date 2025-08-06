17080e26d36e67a735d505d60e87feb0e8171c52
ld3z <ldeziangamer@gmail.com>
2025-08-05 22:15:35 -0700

    moar strats

--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       collections: {
         mdi: () => import('@iconify/json/json/mdi.json').then(i => i.default),
         heroicons: () => import('@iconify/json/json/heroicons.json').then(i => i.default),
+        bitcoinicons: () => import('@iconify/json/json/bitcoinicons.json').then(i => i.default),
       }
     })
   ],
