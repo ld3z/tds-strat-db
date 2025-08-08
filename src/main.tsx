10af461075f0762125a065535982dbaa59ab1912
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-08-07 20:41:03 -0700

    switch to hash based routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppWrapper from './App.tsx';
 import '@unocss/reset/tailwind.css';
 import 'uno.css';
@@ -8,8 +8,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/tds-strat-db/">
+    <HashRouter>
       <AppWrapper />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
